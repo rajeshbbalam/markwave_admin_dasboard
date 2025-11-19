c7fdb986c10d841b50315c003f4b8d1644c899a8
Rajesh Balam <rajeshbbalam@gmail.com>
2025-11-20 05:27:33 +0530

    allowed cors

--- backend/main.py
+++ backend/main.py
@@ -26,7 +26,7 @@ app = FastAPI()
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React dev server
+    allow_origins=["*"],  # Replace "*" with specific origins if needed
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
